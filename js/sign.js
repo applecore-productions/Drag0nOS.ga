bbe7637bb682b7fbe5b5fba3e5517ef7c453dc7e
Dragon <92691789+PoisonDragon704@users.noreply.github.com>
2022-09-04 13:23:57 -0500

    Update sign.js

--- js/sign.js
+++ js/sign.js
@@ -3,5 +3,5 @@ var mobileprovision = "7238a910232e"
 var password = "AppleP12.com"
 
 function IPOGO() {
-window.open("https://sign.starfiles.co/?ipa=1bca09a52a24&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+window.open("https://sign.starfiles.co/?ipa=e0ff4b561288&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
 }
