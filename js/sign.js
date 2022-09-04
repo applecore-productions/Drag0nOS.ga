f08354744c7d9b62f5d6142a5425f07669e8c7f2
Dragon <92691789+PoisonDragon704@users.noreply.github.com>
2022-09-04 11:04:18 -0500

    Create sign.js

---
+++ js/sign.js
@@ -0,0 +1,7 @@
+var p12 = "06f806f66e88"
+var mobileprovision = "7238a910232e"
+var password = "AppleP12.com"
+
+function IPOGO() {
+window.open("https://sign.starfiles.co/?ipa=1bca09a52a24&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
+}
