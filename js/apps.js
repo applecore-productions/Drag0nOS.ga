0a7c5747a15bca5fbce262488e2aa881b613b7f7
Dragon <92691789+PoisonDragon704@users.noreply.github.com>
2022-12-07 12:43:10 -0600

    Update apps.js

--- js/apps.js
+++ js/apps.js
@@ -1,6 +1,6 @@
 var p12 = "77828b019780"
 var mobileprovision = "c0f9f5563202"
-var password = "123"
+var password = "AppleP12.com"
 
 function unc0ver() {
 window.open("https://sign.starfiles.co/?ipa=1bca09a52a24&p12=" + p12 + "&mobileprovision=" + mobileprovision + "&password=" + password + "&redirect=true","_self")
